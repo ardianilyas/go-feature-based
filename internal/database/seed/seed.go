3d96ae431114f0cc054c40c6bce6a54a42df4af8
ardianilyas <ardianilyas@gmail.com>
2025-08-24 16:49:38 +0700

    feat: add post, user seeder, and drop database

--- internal/database/seed/seed.go
+++ internal/database/seed/seed.go
@@ -1,5 +1,16 @@
 package seed
 
+import (
+	"github.com/ardianilyas/go-feature-based/config"
+)
+
+func ResetTables() {
+	config.DB.Exec("TRUNCATE TABLE categories, posts, users RESTART IDENTITY CASCADE")
+}
+
 func RunSeeders() {
+	ResetTables()
+	SeedUser()
 	SeedCategory(50)
+	SeedPost(40)
 }
